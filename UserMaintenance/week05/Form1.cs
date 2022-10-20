3e097090bb950651ca29e24819484c48f997f5a7
Zoltán Szabó <zoltan.szabo4@stud.uni-corvinus.hu>
2022-10-20 16:43:29 +0200

    3. Feladat

--- UserMaintenance/week05/Form1.cs
+++ UserMaintenance/week05/Form1.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week05.MnbServiceReference;
+using System.IO;
 
 namespace week05
 {
     public partial class Form1 : Form
     {
+        MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
         public Form1()
         {
+            Arfolyamleker();
             InitializeComponent();
+        }
+        public void Arfolyamleker()
+        {
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+            request.currencyNames = "EUR";
+            request.startDate = "2020-01-01";
+            request.endDate = "2020-06-30";
+            var response = mnbService.GetExchangeRates(request);
+            string result = response.GetExchangeRatesResult;
+            using (StreamWriter sw = new StreamWriter("result.xml"))
+            {
+                sw.Write(result);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
