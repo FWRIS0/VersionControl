443c07368411df53a606e11567830531ec47210c
Zoltán Szabó <zoltan.szabo4@stud.uni-corvinus.hu>
2022-10-20 17:17:16 +0200

    5. feladat

--- UserMaintenance/week05/Form1.cs
+++ UserMaintenance/week05/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using week05.MnbServiceReference;
 using System.IO;
 using week05.Entities;
+using System.Xml;
 
 namespace week05
 {
@@ -17,6 +18,7 @@ namespace week05
     {
         MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
         BindingList<RateData> Rates = new BindingList<RateData>();
+        XmlDocument xml = new XmlDocument();
         public Form1()
         {
             Arfolyamleker();
@@ -31,9 +33,28 @@ namespace week05
             request.endDate = "2020-06-30";
             var response = mnbService.GetExchangeRates(request);
             string result = response.GetExchangeRatesResult;
+            XMLFeldolg(result);
             using (StreamWriter sw = new StreamWriter("result.xml"))
             {
                 sw.Write(result);
+            }
+        }
+        public void XMLFeldolg(string result)
+        {
+            xml.LoadXml(result);
+            foreach (XmlElement x in xml.DocumentElement)
+            {
+                var child =  (XmlElement)x.ChildNodes[0];
+                RateData rd = new RateData()
+                {
+                    Date = Convert.ToDateTime(x.GetAttribute("date")),
+                    Currency = child.GetAttribute("curr")
+                };
+                if (int.Parse(child.GetAttribute("unit")) != 0)
+                {
+                    rd.Value = decimal.Parse(child.InnerText) / int.Parse(child.GetAttribute("unit"));
+                }
+                Rates.Add(rd);
             }
         }
 
