90bf351634bcd3e497bfca972bd6c63537f34dd9
Zoltán Szabó <zoltan.szabo4@stud.uni-corvinus.hu>
2022-10-26 16:38:10 +0200

    Futószalag létrehozása

--- UserMaintenance/week06/Entities/Ball.cs
+++ UserMaintenance/week06/Entities/Ball.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 
 namespace week06
 {
-    class Ball : Label
+    public class Ball : Label
     {
         public Ball()
         {
--- UserMaintenance/week06/Form1.cs
+++ UserMaintenance/week06/Form1.cs
@@ -7,14 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06.Entities;
 
 namespace week06
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+        
+        private BallFactory _factory;
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            Ball b = Factory.CreateNew();
+            b.Left = -b.Width;
+            _balls.Add(b);
+            mainpanel.Controls.Add(b);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            int max = 0;
+            foreach (var b in _balls)
+            {
+                b.MoveBall();
+                if (max<b.Left)
+                {
+                    max = b.Left;
+                }
+            }
+            if (max>=1000)
+            {
+                mainpanel.Controls.Remove(_balls[0]);
+                _balls.Remove(_balls[0]);
+            }
         }
     }
 }
