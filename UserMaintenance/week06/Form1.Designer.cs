35f990b7cd6eebfce52ede87b4c377b9e67f9d92
Zoltán Szabó <zoltan.szabo4@stud.uni-corvinus.hu>
2022-10-26 17:12:01 +0200

    Gombok

--- UserMaintenance/week06/Form1.Designer.cs
+++ UserMaintenance/week06/Form1.Designer.cs
@@ -33,6 +33,9 @@ namespace week06
             this.mainpanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.btncar = new System.Windows.Forms.Button();
+            this.btnball = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // mainpanel
@@ -40,9 +43,9 @@ namespace week06
             this.mainpanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.mainpanel.Location = new System.Drawing.Point(12, 12);
+            this.mainpanel.Location = new System.Drawing.Point(12, 60);
             this.mainpanel.Name = "mainpanel";
-            this.mainpanel.Size = new System.Drawing.Size(776, 426);
+            this.mainpanel.Size = new System.Drawing.Size(776, 378);
             this.mainpanel.TabIndex = 0;
             // 
             // createTimer
@@ -57,16 +60,50 @@ namespace week06
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
             // 
+            // btncar
+            // 
+            this.btncar.Location = new System.Drawing.Point(12, 12);
+            this.btncar.Name = "btncar";
+            this.btncar.Size = new System.Drawing.Size(105, 42);
+            this.btncar.TabIndex = 1;
+            this.btncar.Text = "CAR";
+            this.btncar.UseVisualStyleBackColor = true;
+            this.btncar.Click += new System.EventHandler(this.btncar_Click);
+            // 
+            // btnball
+            // 
+            this.btnball.Location = new System.Drawing.Point(123, 12);
+            this.btnball.Name = "btnball";
+            this.btnball.Size = new System.Drawing.Size(100, 42);
+            this.btnball.TabIndex = 2;
+            this.btnball.Text = "BALL";
+            this.btnball.UseVisualStyleBackColor = true;
+            this.btnball.Click += new System.EventHandler(this.btnball_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Coming next:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnball);
+            this.Controls.Add(this.btncar);
             this.Controls.Add(this.mainpanel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,6 +112,9 @@ namespace week06
         private System.Windows.Forms.Panel mainpanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Button btncar;
+        private System.Windows.Forms.Button btnball;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- UserMaintenance/week06/Form1.cs
+++ UserMaintenance/week06/Form1.cs
@@ -7,20 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06.Abstractions;
 using week06.Entities;
 
 namespace week06
 {
     public partial class Form1 : Form
     {
-        List<Ball> _balls = new List<Ball>();
+        List<Toy> _toys = new List<Toy>();
         
-        private BallFactory _factory;
-        public BallFactory Factory
+        private IToyFactory _factory;
+        public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set { _factory = value;
+                DisplayNext();
+            }
         }
+
+        Toy _nextToy;
 
 
         public Form1()
@@ -36,28 +41,55 @@ namespace week06
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
-            Ball b = Factory.CreateNew();
-            b.Left = -b.Width;
-            _balls.Add(b);
-            mainpanel.Controls.Add(b);
+            Toy t = Factory.CreateNew();
+            t.Left = -t.Width;
+            _toys.Add(t);
+            mainpanel.Controls.Add(t);
         }
 
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             int max = 0;
-            foreach (var b in _balls)
+            foreach (var t in _toys)
             {
-                b.MoveBall();
-                if (max<b.Left)
+                t.MoveToy();
+                if (max<t.Left)
                 {
-                    max = b.Left;
+                    max = t.Left;
                 }
             }
             if (max>=1000)
             {
-                mainpanel.Controls.Remove(_balls[0]);
-                _balls.Remove(_balls[0]);
+                mainpanel.Controls.Remove(_toys[0]);
+                _toys.Remove(_toys[0]);
             }
+        }
+
+        public void DisplayNext()
+        {
+            if (_nextToy != null)
+            {
+                this.Controls.Remove(_nextToy);
+            }
+            _nextToy = Factory.CreateNew();
+            _nextToy.Left = label1.Left + label1.Width;
+            _nextToy.Top = label1.Top/4;
+            this.Controls.Add(_nextToy);
+        }
+
+        private void btncar_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void btnball_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
