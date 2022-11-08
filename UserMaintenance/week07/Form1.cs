21949bb17db3dc19ba482cdb7c0e4776520b4295
Zoltán Szabó <zoltan.szabo4@stud.uni-corvinus.hu>
2022-11-08 17:45:14 +0100

    7. feladat

--- UserMaintenance/week07/Form1.cs
+++ UserMaintenance/week07/Form1.cs
@@ -30,6 +30,20 @@ namespace week07
             portfolioItems.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
             dataGridView2.DataSource = portfolioItems;
         }
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in portfolioItems)
+            {
+                var last = (from x in ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
