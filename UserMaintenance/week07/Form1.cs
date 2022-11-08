c685de6042f3c825551626e3b3190ce2a84dde42
Zoltán Szabó <zoltan.szabo4@stud.uni-corvinus.hu>
2022-11-08 17:30:19 +0100

    4. feladat

--- UserMaintenance/week07/Form1.cs
+++ UserMaintenance/week07/Form1.cs
@@ -12,9 +12,18 @@ namespace week07
 {
     public partial class Form1 : Form
     {
+        List<Tick> ticks;
+        PortfolioEntities context = new PortfolioEntities();
         public Form1()
         {
             InitializeComponent();
+            ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = ticks;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
