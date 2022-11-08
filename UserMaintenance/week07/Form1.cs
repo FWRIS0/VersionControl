57a368f7ef3be9124a0a1df50a8b43f7478ece32
Zoltán Szabó <zoltan.szabo4@stud.uni-corvinus.hu>
2022-11-08 17:35:57 +0100

    5. feladat

--- UserMaintenance/week07/Form1.cs
+++ UserMaintenance/week07/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week07.Entities;
 
 namespace week07
 {
@@ -14,11 +15,20 @@ namespace week07
     {
         List<Tick> ticks;
         PortfolioEntities context = new PortfolioEntities();
+        List<PortfolioItem> portfolioItems = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
             ticks = context.Ticks.ToList();
             dataGridView1.DataSource = ticks;
+            CreatePortfolio();
+        }
+        public void CreatePortfolio()
+        {
+            portfolioItems.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            portfolioItems.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+            portfolioItems.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            dataGridView2.DataSource = portfolioItems;
         }
 
         private void Form1_Load(object sender, EventArgs e)
