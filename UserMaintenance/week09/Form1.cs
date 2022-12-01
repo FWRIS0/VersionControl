a357544a25384e8709bef43041256d461951ba7e
Zoltán Szabó <zoltan.szabo4@stud.uni-corvinus.hu>
2022-12-01 17:23:08 +0100

    Szimuláció

--- UserMaintenance/week09/Form1.cs
+++ UserMaintenance/week09/Form1.cs
@@ -14,6 +14,7 @@ namespace week09
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random(1234);
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
@@ -21,9 +22,54 @@ namespace week09
         {
             InitializeComponent();
 
-            Population = PersonRead(@"C:\Temp\nép.csv");
+            Population = PersonRead(@"C:\Temp\nép-teszt.csv");
             BirthProbabilities = BirthProbabilityRead(@"C:\Temp\születés.csv");
             DeathProbabilities = DeathProbabilityRead(@"C:\Temp\halál.csv");
+
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    SimStep(year, Population[i]);
+                }
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
+        }
+
+        public void SimStep(int Year, Person person)
+        {
+            if (!person.IsAlive) return;
+
+            int Age = Year - person.BirthYear;
+
+            double DeathProb = (from d in DeathProbabilities
+                                where d.Age == Age && d.Gender == person.Gender
+                                select d.Probability).FirstOrDefault();
+
+            if (rng.NextDouble()<=DeathProb) person.IsAlive = false;
+
+            if (!person.IsAlive && person.Gender == Gender.Male) return;
+
+            double BirthProb = (from b in BirthProbabilities
+                                where b.Age == Age && b.NumberOfChildren == person.NumberOFChildren
+                                select b.Probability).FirstOrDefault();
+
+            if (rng.NextDouble()<=BirthProb)
+            {
+                Population.Add(new Person()
+                {
+                    BirthYear = Year,
+                    Gender = (Gender)Enum.Parse(typeof(Gender), rng.Next(1, 3).ToString()),
+                    NumberOFChildren = 0
+                });
+            }
         }
 
         public List<Person> PersonRead(string Path)
